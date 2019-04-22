29eb36aaef1a692410d7dfe3a5929a7e39fba5b5
Dave <dave.e.siegel@gmail.com>
2019-04-22 11:55:50 -0400

    pager works on /users. need to disable buttons, then handle search results paging

--- src/components/Pager.tsx
+++ src/components/Pager.tsx
@@ -13,8 +13,11 @@ const Pager = (props: PagerProps) => {
         <div>
             {count} Results. Page {+match.params.pageId + 1 || 1} out of {Math.ceil(count/50)}
             <br />
-            <div className="btn-group">
+            <div className="btn-group" style={{ marginTop: "10px", marginBottom: "5px" }}>
             <button type="button" className="btn btn-info" onClick={() => history.push(`/users`)} >First</button>
+            <button type="button" className="btn btn-info" onClick={() => history.push(`/users/${+match.params.pageId - 1}`)} >Prev</button>
+            <button type="button" className="btn btn-primary" onClick={() => history.push(`/users/${+match.params.pageId || 1}`)} >{+match.params.pageId + 1 || 1}</button>
+            <button type="button" className="btn btn-info" onClick={() => history.push(`/users/${+match.params.pageId + 1 || 1}`)} >Next</button>
             <button type="button" className="btn btn-info" onClick={() => history.push(`/users/${Math.ceil(count/50) - 1}`)} >Last</button>
             </div>
         </div>
